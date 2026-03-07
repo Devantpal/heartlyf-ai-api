ec849a73d7eb4e5db92ef8d2c083a642f2365aae
Devant <devant.pal@gmail.com>
2026-03-08 00:07:53 +0530

    init commit

---
+++ app.py
@@ -0,0 +1,27 @@
+from fastapi import FastAPI
+import numpy as np
+import tensorflow as tf
+
+app = FastAPI()
+
+model = tf.keras.models.load_model("ecg_model.h5")
+
+@app.get("/")
+def home():
+    return {"message": "Heartlyf ECG AI API Running"}
+
+@app.post("/predict")
+def predict(data: list):
+
+    arr = np.array(data).reshape(1,720,1)
+
+    prediction = model.predict(arr)
+
+    result = int(np.argmax(prediction))
+
+    confidence = float(np.max(prediction))
+
+    return {
+        "prediction": result,
+        "confidence": confidence
+    }
