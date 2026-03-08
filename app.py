aa3c389f5b9c47a488ba226a4c9a2e6c3c15e88b
Devant <devant.pal@gmail.com>
2026-03-08 11:39:38 +0530

    Change the ECG model

--- app.py
+++ app.py
@@ -1,41 +1,27 @@
-import os
 import numpy as np
 import tensorflow as tf
 from fastapi import FastAPI
 
 app = FastAPI()
 
-model = None
+print("Loading ECG model...")
 
-@app.on_event("startup")
-def load_model():
-    global model
-    try:
-        print("Loading ECG model...")
-        model = tf.keras.models.load_model("ecg_model_clean.keras", compile=False)
-        print("Model loaded successfully")
-    except Exception as e:
-        print("MODEL LOAD ERROR:", e)
+# Load TensorFlow SavedModel
+model = tf.saved_model.load("ecg_saved_model")
 
+infer = model.signatures["serve"]
+
+print("Model loaded successfully")
 
 @app.get("/")
 def home():
-    return {"status": "API running"}
-
+    return {"status": "ECG AI API running"}
 
 @app.post("/predict")
 def predict(ecg_signal: list):
 
-    if model is None:
-        return {"error": "Model not loaded"}
+    data = np.array(ecg_signal).reshape(1,720,1).astype("float32")
 
-    try:
-        data = np.array(ecg_signal)
-        data = data.reshape(1,720,1)
+    prediction = infer(tf.constant(data))
 
-        prediction = model.predict(data)
-
-        return {"prediction": prediction.tolist()}
-
-    except Exception as e:
-        return {"error": str(e)}
+    return {"prediction": str(prediction)}
